10b8377c090ce60dbee42268f0faaa3dad0af8a1
Allan Alderson <adsaalderson@gmail.com>
2023-04-17 12:55:16 +1000

    Bug fix in range.

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ from pygame.locals import *
 pygame.init()
 pygame.display.set_caption("Chook Raffle Video Display")
 RANGE_MIN = 1
-RANGE_MAX = 1111
-FPS = 15
+RANGE_MAX = 1100
+FPS = 20
 FramePerSec = pygame.time.Clock()
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
-DARK = (0, 0, 0)
+BACKGROUND_COLOR = (0, 0, 0)
 GOLD = (250, 190, 80)
 GREY = (180, 180, 190)
-BLACK = (0, 0, 0)
 WHITE = (250, 250, 255)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 winner_peek = 0
@@ -28,26 +27,23 @@ for i in range(10):
 surface_grey = []
 for i in range(10):
     surface_grey.append(my_font.render(str(i), True, GREY))
-surface_dark = []
-for i in range(10):
-    surface_dark.append(my_font.render(str(i), True, DARK))
+
 
 rect_width = surface_grey[0].get_width()
 rect_height = surface_grey[0].get_height()
 rect_surface = pygame.Surface((rect_width, rect_height))
-rect_surface.fill((0, 0, 0))  # Fill with blue color
+rect_surface.fill(BACKGROUND_COLOR)
 # rect_surface.set_alpha(128)
 
 class Screendigit:
-    def __init__(self, x_pos, y_pos, winning_digit, suspence_digit, displayed_digit, resolved, reveal_timer):
+    def __init__(self, x_pos, y_pos, winning_digit, suspense_digit, displayed_digit, resolved, reveal_timer):
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.winning_digit = winning_digit
-        self.suspence_digit = suspence_digit
+        self.suspense_digit = suspense_digit
         self.displayed_digit = displayed_digit
         self.resolved = resolved
         self.reveal_timer = reveal_timer
-
 
 # Create an empty list to store instance position values
 digit_position = []
@@ -58,7 +54,7 @@ for i in range(4):
     SingleDigit.x_pos = 1300 - (i * 400)
     SingleDigit.y_pos = 200
     SingleDigit.winning_digit = 0
-    SingleDigit.suspence_digit = 0
+    SingleDigit.suspense_digit = 0
     SingleDigit.displayed_digit = 0
     SingleDigit.resolved = False
     SingleDigit.reveal_timer = int(time.time() -1)
@@ -70,8 +66,7 @@ def draw_winning_ticket():
     """  Gets the winning ticket. Also splits this winning number into relevant positions (units, tens, hundreds, thousands) """
     global winner_known
     if winner_known == False:
-        winning_ticket = random.randint(RANGE_MIN, RANGE_MAX + 1)
-        winning_ticket = 8
+        winning_ticket = random.randint (RANGE_MIN, RANGE_MAX)
         if winning_ticket > 9999:
             winning_ticket = 0
         if winning_ticket < 0:
@@ -84,22 +79,21 @@ def draw_winning_ticket():
         winner_known = True
 
 
-def draw_suspence_ticket():
-    global suspence_ticket
-    suspence_ticket = random.randint(RANGE_MIN, RANGE_MAX + 1)
-    digit_position[0].suspence_digit = (suspence_ticket // 1 % 10)
-    digit_position[1].suspence_digit = (suspence_ticket // 10 % 10)
-    digit_position[2].suspence_digit = (suspence_ticket // 100 % 10)
-    digit_position[3].suspence_digit = (suspence_ticket // 1000 % 10)
-
+def draw_suspense_ticket():
+    global suspense_ticket
+    suspense_ticket = random.randint(RANGE_MIN, RANGE_MAX)
+    digit_position[0].suspense_digit = (suspense_ticket // 1 % 10)
+    digit_position[1].suspense_digit = (suspense_ticket // 10 % 10)
+    digit_position[2].suspense_digit = (suspense_ticket // 100 % 10)
+    digit_position[3].suspense_digit = (suspense_ticket // 1000 % 10)
 
 
 
 def start_reveal_timers():
-    digit_position[3].reveal_timer = int(time.time() + 0)  # 0
+    digit_position[3].reveal_timer = int(time.time() + -1)  # 0
     digit_position[2].reveal_timer = int(time.time() + 1)  # 1
-    digit_position[1].reveal_timer = int(time.time() + 2)  # 4
-    digit_position[0].reveal_timer = int(time.time() + 3)  # 8
+    digit_position[1].reveal_timer = int(time.time() + 4)  # 4
+    digit_position[0].reveal_timer = int(time.time() + 7)  # 8
 
 
 
@@ -112,7 +106,7 @@ def update_logic():
     if spin_stopped == False:
         for i in range(4):
             if digit_position[i].resolved == False:
-                digit_position[i].displayed_digit =  digit_position[i].suspence_digit # random.randint(0, 9)
+                digit_position[i].displayed_digit =  digit_position[i].suspense_digit # random.randint(0, 9)
         if all([digit_position[0].resolved, digit_position[1].resolved, digit_position[2].resolved,
                 digit_position[3].resolved]):
             spin_stopped = True
@@ -120,7 +114,7 @@ def update_logic():
             print()
             update_display()
             time.sleep(1)
-            pygame.event.get()  # removes superfulous key strokes from buffer
+            pygame.event.get()  # removes key strokes from buffer
     if winner_known == True:
         for i in range(4):
             if int(time.time()) > digit_position[i].reveal_timer:
@@ -133,26 +127,24 @@ def update_logic():
 def update_display():
     ''' Draws  displayed_digit with a grey surface then removes leading zeros. '''
     global winner_peek
-    global suspence_ticket
-    screen.fill(BLACK)
-
+    global suspense_ticket
+    screen.fill(BACKGROUND_COLOR)
     for i in range(4):
         screen.blit(surface_grey[digit_position[i].displayed_digit], (digit_position[i].x_pos, digit_position[i].y_pos))
-    # Erase the leading zeros of display
-
+    # Erase the leading zeros of the suspense_ticket blit with a rectangle
         if digit_position[3].resolved == False:
-            if suspence_ticket < 1000:
-                if digit_position[3].suspence_digit == 0:
+            if suspense_ticket < 1000:
+                if digit_position[3].suspense_digit == 0:
                     screen.blit(rect_surface, (digit_position[3].x_pos, digit_position[3].y_pos))
         if digit_position[2].resolved == False:
-            if suspence_ticket < 100:
-                if digit_position[2].suspence_digit == 0:
+            if suspense_ticket < 100:
+                if digit_position[2].suspense_digit == 0:
                     screen.blit(rect_surface, (digit_position[2].x_pos, digit_position[2].y_pos))
         if digit_position[1].resolved == False:
-            if suspence_ticket < 10:
-                if digit_position[1].suspence_digit == 0:
+            if suspense_ticket < 10:
+                if digit_position[1].suspense_digit == 0:
                     screen.blit(rect_surface, (digit_position[1].x_pos, digit_position[1].y_pos))
-
+        # Erase the leading zeros of the winners blit with a rectangle
         if digit_position[3].resolved:
             if winner_peek < 1000:
                 screen.blit(rect_surface, (digit_position[3].x_pos, digit_position[3].y_pos))
@@ -164,22 +156,18 @@ def update_display():
                 screen.blit(rect_surface, (digit_position[1].x_pos, digit_position[1].y_pos))
 
 
-
-
 def spin():
     '''marks all digits to resolved False'''
     global spin_stopped
     global winner_known
-
     print("      SPINNING . . .")
-    spin_stopped = False
-    winner_known = False
     for i in range(4):
         digit_position[i].resolved = False
+    spin_stopped = False
+    winner_known = False
 
 
-draw_suspence_ticket()
-#draw_winning_ticket()
+draw_suspense_ticket()
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -193,7 +181,7 @@ while True:
                     print("   SLOWING TO", winner_peek)
                 elif spin_stopped:
                     spin()
-    draw_suspence_ticket()
+    draw_suspense_ticket()
     update_logic()
     update_display()
     pygame.display.flip()
